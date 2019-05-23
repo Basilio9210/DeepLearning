d4895d6a3087f87ead26224c4c3c73b72a923439
Basilio Saldarriaga <basilio9210@gmail.com>
2019-05-23 08:00:50 -0500

    Update LSTM.py

--- LSTM.py
+++ LSTM.py
@@ -106,3 +106,82 @@ accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
 
 # Initializing the variables
 init = tf.global_variables_initializer()
+
+# Launch the graph
+with tf.Session() as session:
+    session.run(init)
+    step = 0
+    offset = random.randint(0,n_input+1)
+    end_offset = n_input + 1
+    acc_total = 0
+    loss_list = 0
+
+    #writer.add_graph(session.graph)
+
+    # while step < training_iters:
+    for epoch_idx in range(num_epochs): #
+        # Generate a minibatch. Add some randomness on selection process.
+        if offset > (len(training_data)-end_offset):
+            offset = random.randint(0, n_input+1)
+        
+        # Define the input words per batch
+        symbols_in_keys = [ [dictionary[ str(training_data[i])]] for i in range(offset, offset+n_input) ]
+        symbols_in_keys = np.reshape(np.array(symbols_in_keys), [-1, n_input, 1])
+        
+        # Define the label of words per batch
+        symbols_out_onehot = np.zeros([vocab_size], dtype=float)
+        symbols_out_onehot[dictionary[str(training_data[offset+n_input])]] = 1.0
+        symbols_out_onehot = np.reshape(symbols_out_onehot,[1,-1])
+        
+        # Feed the graph
+        _, acc, loss, onehot_pred = session.run([optimizer, accuracy, total_loss, predictions], \
+                                                feed_dict={batchX_placeholder: _ , \
+                                                           batchY_placeholder: _ })
+        loss_list += loss
+        acc_total += acc
+        if (epoch_idx+1) % echo_step == 0:
+            
+            print("Step = " + str(epoch_idx+1) + ", Loss = " + \
+                  "{:.6f}".format(loss_list/echo_step) + ", Accuracy= " + \
+                  "{:.2f}%".format(100 * acc_total / echo_step))
+            acc_total = 0
+            loss_list = 0
+            symbols_in = [training_data[i] for i in range(offset, offset + n_input)]
+            symbols_out = training_data[offset + n_input]
+            symbols_out_pred = reverse_dictionary[int(tf.argmax(onehot_pred, 1).eval())]
+            print("%s - [%s] vs [%s]" % (symbols_in, symbols_out, symbols_out_pred))
+        #step += 1
+        offset += (n_input+1)
+    
+    print("Optimization Finished!")
+    
+    
+    flag = True
+    while flag == True:
+        prompt = "Write %s words: " % n_input
+        sentence = input(prompt)
+        sentence = sentence.strip()
+        words = sentence.split(' ')
+        
+        if words[0] == '1':
+            flag = False
+            break
+        
+        if len(words) != n_input:
+            print ("Wrong num of words")
+            continue
+        try:
+        #if True:
+            symbols_in_keys = [dictionary[str(words[i])] for i in range(len(words))]
+            for i in range(words_to_predict):
+                keys = np.reshape(np.array(symbols_in_keys), [-1, n_input, 1])
+                onehot_pred = session.run(predictions, feed_dict={batchX_placeholder: keys})
+                onehot_pred_index = int(tf.argmax(onehot_pred, 1).eval())
+                sentence = "%s %s" % (sentence,reverse_dictionary[onehot_pred_index])
+                symbols_in_keys = symbols_in_keys[1:]
+                symbols_in_keys.append(onehot_pred_index)
+            print(sentence)
+            
+        except:
+        #else:
+            print("Word not in dictionary")
