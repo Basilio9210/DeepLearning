d18f507b3f8741568ba77c4313705416a9d99383
Basilio Saldarriaga <basilio9210@gmail.com>
2019-06-05 07:51:55 -0500

    Update Filtro blur.py

--- Filtro blur.py
+++ Filtro blur.py
@@ -4,7 +4,8 @@ import cv2
 # Load an color image in grayscale
 img = cv2.imread('lena.jpg',0)
 
-blur = cv2.blur(img,(5,5)) # Aplicar filtro de suavizado a la imagen
+blur = cv2.blur(img,(5,5)) 
+# Aplicar filtro de suavizado a la imagen
 
 cv2.imshow('image',img)
 cv2.imshow('filtrada',blur)
